f9a98400b987536bd9b041a45e78c55bf68261ff
jsugim <jsugim@39degreesc.com>
2021-10-07 18:25:05 +0900

    TSJ-17 프로젝트 생성

---
+++ test.jira/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test.jira
+//
+//  Created by 김지수 on 2021/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
